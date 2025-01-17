9708af21223c659dd62837db5d40ba1a052f60aa
Awes Khan Ayyub Khan <44608315+Awes-Khan@users.noreply.github.com>
2025-01-17 17:18:56 +0530

    updated

--- src/components/UsersItem.jsx
+++ src/components/UsersItem.jsx
@@ -1,23 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 const UsersItem = ( {data} ) => {
-    const { id } = useParams();
-    const user = data[id-1];
-    // console.log(id, data[id-1],Object.keys(user));
+  const { id } = useParams();
+  const user = data[id - 1];
+  const [loadingstate, setLoadingstate] = React.useState(true);
+  
+  // console.log(id, data[id-1],Object.keys(user));
+  useEffect(() => {
+    setTimeout(() => {
+      setLoadingstate(false);
+    }, 1000);
+  }, []);
   return (
-    <div>
-      <h1>User Details</h1>
-      <ul>
-      {Object.keys(user).map((id) => (
-        <li key={id}>
-          <p>
-            {id.charAt(0).toUpperCase() + id.slice(1).toLowerCase()}:{user[id]}
-          </p>
-      </li>
-      ))}
-      </ul>
-    </div>
+    loadingstate ? <p>Loading...</p> :
+      <div>
+        <h1>User Details</h1>
+        <ul>
+          {Object.keys(user).map((id) => (
+            <li key={id}>
+              <p>
+                <b>{id.charAt(0).toUpperCase() + id.slice(1).toLowerCase()}:</b> {user[id]}
+              </p>
+            </li>
+          ))}
+        </ul>
+      </div>
   );
 };
 
